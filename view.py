0e7a23117c593a7215594d2676aac7a1341e07a8
rodrigoclira <rodrigocliras@gmail.com>
2022-12-30 16:56:48 -0300

    fixing memory usage

--- view.py
+++ view.py
@@ -13,19 +13,19 @@ class SwarmView():
         self.y = np.arange(xmin, xmax, 0.05)
         self.meshgrid = np.meshgrid(self.x, self.y)
         self.function = function
-        self.fig = plt.figure()
         self.root = ROOT + str(simuid) + os.sep
         self.filename_output = f"_tmp_{simuid}.gif"        
         self.tmp_dir = os.path.join(self.root, '_gifs')
 
     def plot_view(self, positions, iteration, title="", is_3d = False, save = True):
+        fig = plt.figure()
         
         if save:
             if not os.path.exists(self.tmp_dir):
                 os.makedirs(self.tmp_dir)
 
         if not is_3d:
-            ax = self.fig.add_subplot(1, 1, 1)
+            ax = fig.add_subplot(1, 1, 1)
             plot_2d_pso(self.meshgrid, self.function, positions, None, ax=ax)        
             ax.set_title(f"it={iteration}  {title}")
         else:
@@ -38,6 +38,7 @@ class SwarmView():
             plt.show()
         else:
             plt.savefig(save_path)
+            plt.close()
 
     def create_gif(self, remove_folder = False):
         make_gif_from_folder(self.tmp_dir, os.path.join(self.root, self.filename_output), remove_folder)
