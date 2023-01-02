1893735edb5d067d443723a814e25c09afda9fc5
rodrigoclira <rodrigocliras@gmail.com>
2023-01-02 19:45:23 -0300

    improving code

--- view.py
+++ view.py
@@ -8,7 +8,7 @@ ROOT = 'tmp/'
 save = True
 
 class SwarmView():
-    def __init__(self, simuid, xmin, xmax, function):
+    def __init__(self, simuid, xmin, xmax, function, is_3d = False):
         self.x = np.arange(xmin, xmax, 0.05)
         self.y = np.arange(xmin, xmax, 0.05)
         self.meshgrid = np.meshgrid(self.x, self.y)
@@ -16,19 +16,25 @@ class SwarmView():
         self.root = ROOT + str(simuid) + os.sep
         self.filename_output = f"_tmp_{simuid}.gif"        
         self.tmp_dir = os.path.join(self.root, '_gifs')
+        self.is_3d = is_3d
+        self.projection = None
+        
+        if self.is_3d:
+            self.projection = '3d'
 
-    def plot_view(self, positions, iteration, title="", is_3d = False, save = True):
+
+    def plot_view(self, positions, iteration, title="", save = True):
         fig = plt.figure()
         
         if save:
             if not os.path.exists(self.tmp_dir):
                 os.makedirs(self.tmp_dir)
+        
+        ax = fig.add_subplot(1, 1, 1, projection=self.projection)  
 
-        if not is_3d:
-            ax = fig.add_subplot(1, 1, 1)
+        if not self.is_3d:
             plot_2d_pso(self.meshgrid, self.function, positions, None, ax=ax)        
-        else:
-            ax = fig.add_subplot(1, 1, 1, projection='3d')  
+        else:            
             plot_3d_pso(self.meshgrid, self.function, positions, None, ax=ax)
 
         ax.set_title(f"it={iteration}  {title}")
@@ -45,7 +51,7 @@ class SwarmView():
 
 if __name__ == '__main__':
 
-    sv = SwarmView("teste", -5, 5, lambda x, y: x**2 + y**2)
+    sv = SwarmView(simuid = "teste", xmin = -5, xmax = 5, function = lambda x, y: x**2 + y**2, is_3d=False, )
 
-    sv.plot_view(np.array([[2, 2], [0, 0], [-1, 1]]), 0, is_3d=False, save = True)
+    sv.plot_view(np.array([[2, 2], [0, 0], [-1, 1]]), 0, save = True)
     sv.create_gif()
